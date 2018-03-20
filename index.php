62980d163b0b79cee425354fc996ae5edd0eb7a7
Yvette Johnson-Rodgers <skyperson15@gmail.com>
2018-03-19 21:30:58 -0600

    added starter index page

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en"
+      <head>
+          <meta charset="UTF-8"/>
+          <title> I complete the Prework</title>
+      </head>
+<body>
+<p>I completed the prework!</p>
+</body>
+      </html>
